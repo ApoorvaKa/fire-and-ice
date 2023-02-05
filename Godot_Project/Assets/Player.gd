278839e083df4979763b869d16a750171ebc8ccc
Apoorva Kaushik <apoorva.kaushik@gmail.com>
2023-02-05 11:43:09 -0500

    player acceleration added

--- Godot_Project/Assets/Player.gd
+++ Godot_Project/Assets/Player.gd
@@ -1,27 +1,33 @@
 extends KinematicBody2D
 
-export (int) var speed = 200
+var speed: float = 200
+var acceleration: float = 2000 # this is 10 times the speed
+var friction: float = acceleration / speed
 
-var velocity = Vector2()
-var screen_size
+var velocity: Vector2 = Vector2()
 
-func _ready():
-	screen_size = get_viewport_rect().size
+func _process(delta: float) -> void:
+	apply_traction(delta)
+	apply_friction(delta)
 
-func get_input():
-	velocity = Vector2()
-	if Input.is_action_pressed("right"):
-		velocity.x += 1
-	if Input.is_action_pressed("left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("down"):
-		velocity.y += 1
-	if Input.is_action_pressed("up"):
-		velocity.y -= 1
-	velocity = velocity.normalized() * speed
-
-func _physics_process(delta):
-	get_input()
+func _physics_process(delt: float) -> void:
 	velocity = move_and_slide(velocity)
-	position.x = clamp(position.x, 0, screen_size.x) # clamp to keep it within the screen
-	position.y = clamp(position.y, 0, screen_size.y) # clamp to keep it within the screen
+	
+func apply_traction(delta: float) -> void:
+	var traction: Vector2 = Vector2()
+	
+	if (Input.is_action_pressed("left")):
+		traction.x -= 1
+	if (Input.is_action_pressed("right")):
+		traction.x += 1
+	if (Input.is_action_pressed("up")):
+		traction.y -= 1
+	if (Input.is_action_pressed("down")):
+		traction.y += 1
+	
+	traction = traction.normalized()
+	
+	velocity += traction * acceleration * delta
+	
+func apply_friction(delta: float) -> void:
+	velocity -= velocity * friction * delta
